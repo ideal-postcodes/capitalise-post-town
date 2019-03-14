a28748676317c70c4de8c96292686067e81c04d6
Christopher Blanchard <cablanchard@gmail.com>
2019-03-14 14:06:43 +0100

    Add pretter dev dependency; Apply new standards

--- lib/index.ts
+++ lib/index.ts
@@ -5,11 +5,11 @@ const containsAmpersand = /\w+&\w+/;
 
 // capitalise word with exceptions on exclusion list
 const capitaliseWord = (word: string): string => {
-	word = word.toLowerCase();
-	if (word.match(exclusion)) return word;
-	if (word.match(containsAmpersand)) return word.toUpperCase();
+  word = word.toLowerCase();
+  if (word.match(exclusion)) return word;
+  if (word.match(containsAmpersand)) return word.toUpperCase();
 
-	return word.charAt(0).toUpperCase() + word.slice(1);
+  return word.charAt(0).toUpperCase() + word.slice(1);
 };
 
 const joiner = /-/;
@@ -17,32 +17,32 @@ const joinerWord = /^(in|de|under|upon|y|on|over|the|by)$/;
 
 // Check for names connected with hyphens
 const checkJoins = (string: string): string => {
-	if (string.match(joiner) === null) return string;
+  if (string.match(joiner) === null) return string;
 
-	return string
-		.split("-")
-		.map(str => {
-			if (str.match(joinerWord)) return str.toLowerCase();
+  return string
+    .split("-")
+    .map(str => {
+      if (str.match(joinerWord)) return str.toLowerCase();
 
-			return capitaliseWord(str);
-		})
-		.join("-")
-}
+      return capitaliseWord(str);
+    })
+    .join("-");
+};
 
 const boness = /bo'ness/i;
 
 // Handles unusual names which cannot be easily generalised into a rule
 const exceptions = (str: string): string => {
-	if (str.match(boness)) return "Bo'Ness";
+  if (str.match(boness)) return "Bo'Ness";
 
-	return str;
-}
+  return str;
+};
 
 export const capitalisePostTown = (postTown: string): string => {
-	return postTown
-		.split(" ")
-		.map(capitaliseWord)
-		.map(checkJoins)
-		.map(exceptions)
-		.join(" ");
-}
+  return postTown
+    .split(" ")
+    .map(capitaliseWord)
+    .map(checkJoins)
+    .map(exceptions)
+    .join(" ");
+};
