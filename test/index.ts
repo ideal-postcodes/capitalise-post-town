ff9c7a3c65a6d86011a2ec291a72f56b196303b2
Christopher Blanchard <cablanchard@gmail.com>
2019-03-14 14:06:43 +0100

    Add pretter dev dependency; Apply new standards

--- test/index.ts
+++ test/index.ts
@@ -1,20 +1,22 @@
 "use strict";
 
-import * as fs from "fs";
-import * as path from "path";
+import { readFileSync } from "fs";
+import { join } from "path";
 import { assert } from "chai";
 import { capitalisePostTown } from "../lib/index";
 
-const posttowns = fs.readFileSync(path.join(__dirname, "./post_towns.csv"), { encoding: "utf8" })
-	.split("\n")
-	.map(posttown => posttown.trim());
+const posttowns = readFileSync(join(__dirname, "./post_towns.csv"), {
+  encoding: "utf8"
+})
+  .split("\n")
+  .map(posttown => posttown.trim());
 
 const capitalisedPosttowns = posttowns.map(posttown => posttown.toUpperCase());
 
 describe("capitalise", () => {
-	for (let i = 0; i < posttowns.length; i++) {
-		it (`correctly capitalises ${posttowns[i]}`, () => {
-			assert.equal(capitalisePostTown(capitalisedPosttowns[i]), posttowns[i]);
-		});
-	}
+  for (let i = 0; i < posttowns.length; i++) {
+    it(`correctly capitalises ${posttowns[i]}`, () => {
+      assert.equal(capitalisePostTown(capitalisedPosttowns[i]), posttowns[i]);
+    });
+  }
 });
